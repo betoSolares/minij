4ad7d2bbebdf0965d4dea293bc10635f1c3477aa
Roberto Solares <betosolaresgar@gmail.com>
2020-08-20 09:35:41 -0600

    Check if the file is empty

--- src/core/lexer.py
+++ src/core/lexer.py
@@ -1,3 +1,9 @@
+import sys
+
+
 class Lexer:
     def get_lexemes(self, lines):
-        print("GET Lexemes")
+        # Check if the file is empty
+        if len(lines) == 0:
+            print("There is nothing to do, the file is empty")
+            sys.exit(1)
