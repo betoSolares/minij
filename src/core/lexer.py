76ad66be4f1462726b95e98fb5fca53f0b8bf590
Roberto Solares <betosolaresgar@gmail.com>
2020-08-20 10:00:20 -0600

    Iterate through the elements of each line

--- src/core/lexer.py
+++ src/core/lexer.py
@@ -7,3 +7,8 @@ class Lexer:
         if len(lines) == 0:
             print("There is nothing to do, the file is empty")
             sys.exit(1)
+
+        # Get all the words in the file
+        for line_number, text in lines.items():
+            for char in text:
+                print(line_number, "->", char)
