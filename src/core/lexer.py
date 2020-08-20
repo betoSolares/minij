30f9cf10b9bd5b60757c78650b8b77db70148df5
Roberto Solares <betosolaresgar@gmail.com>
2020-08-20 11:45:36 -0600

    Create basic lexer for simple words

--- src/core/lexer.py
+++ src/core/lexer.py
@@ -2,6 +2,30 @@ import sys
 
 
 class Lexer:
+    def __init__(self):
+        self.__symbols__ = [
+            "+",
+            "-",
+            "*",
+            "/",
+            "%",
+            "<",
+            "=",
+            ">",
+            "!",
+            "&",
+            "|",
+            ";",
+            ",",
+            ".",
+            "[",
+            "]",
+            "(",
+            ")",
+            "{",
+            "}",
+        ]
+
     def get_lexemes(self, lines):
         # Check if the file is empty
         if len(lines) == 0:
@@ -9,6 +33,17 @@ class Lexer:
             sys.exit(1)
 
         # Get all the words in the file
+        word = ""
+
         for line_number, text in lines.items():
             for char in text:
-                print(line_number, "->", char)
+                if char.isspace():
+                    if len(word) > 0:
+                        # Do something with the word
+                        word = ""
+                elif char in self.__symbols__:
+                    if len(word) > 0:
+                        # Do something with the word
+                        word = ""
+                else:
+                    word += char
