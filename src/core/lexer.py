0b34d979f66cc472e4659cbac8f30783f328b6ea
Roberto Solares <betosolaresgar@gmail.com>
2020-08-21 11:43:29 -0600

    Get single and double characters

--- src/core/lexer.py
+++ src/core/lexer.py
@@ -25,6 +25,37 @@ class Lexer:
             "{",
             "}",
         ]
+        self.__single_operator = [
+            "+",
+            "-",
+            "*",
+            "/",
+            "%",
+            "<",
+            ">",
+            "=",
+            "!",
+            ";",
+            ",",
+            ".",
+            "[",
+            "]",
+            "(",
+            ")",
+            "{",
+            "}",
+        ]
+        self.__double_operator__ = [
+            "<=",
+            ">=",
+            "==",
+            "!=",
+            "&&",
+            "||",
+            "[]",
+            "()",
+            "{}",
+        ]
 
     def get_lexemes(self, lines):
         # Check if the file is empty
@@ -34,16 +65,44 @@ class Lexer:
 
         # Get all the words in the file
         word = ""
+        symbol_found = False
 
         for line_number, text in lines.items():
             for char in text:
+
                 if char.isspace():
                     if len(word) > 0:
                         # Do something with the word
                         word = ""
+                        symbol_found = False
+
                 elif char in self.__symbols__:
-                    if len(word) > 0:
+                    # Check if word and symbol are together
+                    # Something like Main. or String[]
+                    if len(word) > 0 and not symbol_found:
                         # Do something with the word
                         word = ""
+
+                    # Verify if is the first or the second symbol
+                    if not symbol_found:
+                        symbol_found = True
+                        word += char
+                    else:
+                        # Recognize double operator
+                        if word + char in self.__double_operator__:
+                            # Do something with the word
+                            symbol_found = False
+                            word = ""
+                        else:
+                            # Do something with the word
+                            symbol_found = True
+                            word = char
+
                 else:
+
+                    if symbol_found:
+                        # Do something with the word
+                        word = ""
+
                     word += char
+                    symbol_found = False
