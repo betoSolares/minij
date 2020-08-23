68423d8f32e888bc406f6e8d152b2eeb002fa91f
Roberto Solares <betosolaresgar@gmail.com>
2020-08-23 10:39:18 -0600

    Impelement help message

--- src/helpers/flags.py
+++ src/helpers/flags.py
@@ -1,9 +1,9 @@
-import sys
-import os
 import getopt
+import os
+import sys
+
 
 class Flags:
-
     def parse_flags(self, arg_list):
         # Try to get all the values passed to the program
         shorts = "ho:q"
@@ -13,6 +13,7 @@ class Flags:
             opts, vals = getopt.getopt(arg_list, shorts, longs)
         except getopt.error as e:
             print("ERROR: %s" % e)
+            print("Try doing minij -h or minij --help to get more information")
             sys.exit(1)
 
         # Default values
@@ -23,15 +24,6 @@ class Flags:
             "quiet": False,
         }
 
-        # Get the input file
-        if len(vals) > 1:
-            print("ERROR: only one file is allowed")
-            sys.exit(1)
-        elif len(vals) < 1:
-            print("ERROR: no file provided")
-            sys.exit(1)
-        args["input_file"] = vals[0]
-
         # Get all the options of the program
         for opt, val in opts:
             if opt in ("-h", "--help"):
@@ -41,8 +33,20 @@ class Flags:
             elif opt in ("-q", "--quiet"):
                 args["quiet"] = True
 
+        # Get the input file
+        if not args["help"]:
+            if len(vals) > 1:
+                print("ERROR: only one file is allowed")
+                sys.exit(1)
+            elif len(vals) < 1:
+                print("ERROR: no file provided")
+                sys.exit(1)
+            args["input_file"] = vals[0]
+
         # Verify that the output file is set
-        if args["output_file"] is None:
-            args["output_file"] = os.path.splitext(args["input_file"])[0] + ".out"
+        if not args["help"] and args["output_file"] is None:
+            args["output_file"] = (
+                os.path.splitext(args["input_file"])[0] + ".out"
+            )
 
         return args
--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 import sys
 
-from helpers import Flags, IO
+from helpers import IO, Flags
+
+
+def help_message():
+    print("usage: minij [OPTIONS] [FILE]\n")
+    print("OPTIONS:")
+    print("  -h, --help      Show help for the command")
+    print("  -o, --output    Specify the oruput file")
+    print("  -q, --quiet     Don't print anything")
+
 
 if __name__ == "__main__":
     flags = Flags()
     args = flags.parse_flags(sys.argv[1:])
+
+    if args["help"]:
+        help_message()
+        sys.exit(0)
+
     io = IO(args["input_file"], args["output_file"])
     lines = io.read()
