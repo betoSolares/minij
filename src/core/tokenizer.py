32b34a3fd2695ea3c47c33e8c156237510fa8ddf
mochisvelas <velasquezmochis@gmail.com>
2020-08-24 11:43:23 -0600

    Fixing linting issues

--- src/core/tokenizer.py
+++ src/core/tokenizer.py
@@ -1,34 +1,40 @@
-import sys
 import re
 
 from .token import Token
-from .lexeme import Lexeme
+
 
 class Tokenizer:
     def __init__(self):
-        self.__reservedwords__ = [
-                "void",
-                "int",
-                "double",
-                "boolean",
-                "string",
-                "class",
-                "const",
-                "interface",
-                "null",
-                "this",
-                "extends",
-                "implements",
-                "for",
-                "while",
-                "if",
-                "else",
-                "return",
-                "break",
-                "New",
-                "System",
-                "out",
-                "printin",
+
+        self.__deci_pattern__ = r"^[0-9]+$"
+        self.__hexa_pattern__ = r"^0[x|X][0-9a-fA-F]+$"
+        self.__double_pattern__ = r"^[0-9]+.[0-9]*([e|E][+|-]?[0-9]+)?$"
+        self.__str_pattern__ = r"^\"[^\"\n]*\"$"
+        self.__id_pattern__ = r"^[a-zA-Z\$][0-9a-zA-Z\$]*$"
+
+        self.__reserved_words__ = [
+            "void",
+            "int",
+            "double",
+            "boolean",
+            "string",
+            "class",
+            "const",
+            "interface",
+            "null",
+            "this",
+            "extends",
+            "implements",
+            "for",
+            "while",
+            "if",
+            "else",
+            "return",
+            "break",
+            "New",
+            "System",
+            "out",
+            "printin",
         ]
         self.__single_operator__ = [
             "+",
@@ -61,59 +67,64 @@ class Tokenizer:
             "()",
             "{}",
         ]
-        self.__deciPattern__ = r"^[0-9]+$"
-        self.__hexaPattern__ = r"^0[x|X][0-9a-fA-F]+$"
-        self.__doublePattern__ = r"^[0-9]+.[0-9]*([e|E][+|-]?[0-9]+)?$"
-        self.__strPattern__ = r"^\"[^\"\n]*\"$"
-        self.__IdPattern_ = r"^[a-zA-Z\$][0-9a-zA-Z\$]*$"
 
     def categorize(self, lexeme_list):
-        #Match all lexems with their category
+        # Match all lexems with their category
         tokens = []
         category = ""
 
-        for lexeme in lexemes_list:
+        for lexeme in lexeme_list:
 
-            #Recognize reserved word
-            if lexeme.word in self.__reservedwords__:
-                category = "T_" + lexeme.word
+            curr_word = lexeme.word()
+            # Recognize reserved word
+            if curr_word in self.__reserved_words__:
+                category = "T_" + curr_word
 
-            #Recognize decimal whole number
-            elif re.search(__deciPattern__, lexeme.word):
-                category = "T_IntConstant_Decimal (value = " + lexeme.word + ")"
+            # Recognize decimal whole number
+            elif re.search(self.__deci_pattern__, curr_word):
+                category = "T_IntConstant_Decimal (value = " + curr_word + ")"
 
-            #Recognize hexadecimal whole number
-            elif re.search(__hexaPattern__, lexeme.word):
-                category = "T_IntConstant_Hexadecimal (value =" + lexeme.word + ")"
+            # Recognize hexadecimal whole number
+            elif re.search(self.__hexa_pattern__, curr_word):
+                category = "T_IntConstant_Hexadecimal (value =" + curr_word + ")"
 
-            #Recognize double constant
-            elif re.search(__doublePattern__, lexeme.word):
-                category = "T_DoubleConstant (value =" + lexeme.word + ")"
+            # Recognize double constant
+            elif re.search(self.__double_pattern__, curr_word):
+                category = "T_DoubleConstant (value =" + curr_word + ")"
 
-            #Recognize string constant
-            elif re.search(__strPattern__, lexeme.word):
+            # Recognize string constant
+            elif re.search(self.__str_pattern__, curr_word):
                 category = "T_StrConstant"
 
-            #Recognize boolean constant
-            elif lexeme.word == "true" or lexeme.word == "false":
+            # Recognize boolean constant
+            elif curr_word == "true" or curr_word == "false":
                 category = "T_BoolConstant"
 
-            #Recognize double operator
-            elif lexeme.word in self.__double_operator__:
+            # Recognize double operator
+            elif curr_word in self.__double_operator__:
                 category = "T_DoubleOperator"
 
-            #Recognize single operator
-            elif lexeme.word in self.__single_operator__:
+            # Recognize single operator
+            elif curr_word in self.__single_operator__:
                 category = "T_SingleOperator"
-            #Recognize indentifier
-            elif re.search(__IdPattern_, lexeme.word):
+            # Recognize indentifier
+            elif re.search(self.__id_pattern__, curr_word):
                 category = "T_Identifier"
 
             else:
                 category = "ERROR"
 
-            tokens.append(self.create_token(lexeme.word, lexeme.line, lexeme.col_start, lexeme.col_finish, category))
-    return tokens
+            tokens.append(
+                self.create_token(
+                    lexeme.word,
+                    lexeme.line,
+                    lexeme.col_start,
+                    lexeme.col_finish,
+                    category,
+                )
+            )
+
+        return tokens
 
     def create_token(self, lexeme, line, col_start, col_finish, category):
         return Token(lexeme, line, col_start, col_finish, category)
