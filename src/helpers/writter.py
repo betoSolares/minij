a3b58bce1a498d24e0e30e8449808086bb580650
mochisvelas <velasquezmochis@gmail.com>
2020-08-25 17:48:15 -0600

    Fixing linting issues

--- src/helpers/writter.py
+++ src/helpers/writter.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+
 
 class Writter:
     def __init__(self, token_list, input):
@@ -7,37 +8,54 @@ class Writter:
         self.input = os.path.splitext(input)[0]
         self.output = "../out" + os.path.splitext(input)[0] + ".out"
 
-
     def write(self, token_obj, lexeme_list):
-        #Try to write to a new output file
+        # Try to write to a new output file
         try:
             if os.path.isfile(self.output):
                 cont = 1
                 new_output = self.input + "(" + cont + ")"
 
-                while not os.path.isfile(self.output)
-                cont++
-                new_output = self.input + "(" + cont + ")"
+                while not os.path.isfile(self.output):
+                    cont += 1
+                    new_output = self.input + "(" + cont + ")"
 
                 self.output = "../out" + new_output + ".out"
 
-
             with open(self.output, "w") as file:
-                    #write file
-                    if not token_obj.errors:
+                # write file
+                if not token_obj.errors:
+                    for token in self.token_list:
+                        file.write(
+                            "{} line {} cols {}-{} is {}".format(
+                                token.lexeme,
+                                token.line,
+                                token.col_start,
+                                token.col_finish,
+                                token.category,
+                            )
+                        )
+
+                else:
+                    for lexeme in lexeme_list:
+                        for error in token_obj.errors:
+                            if error.endswith(lexeme.word):
+                                file.write(
+                                    "*** Error line {} *** {}".format(
+                                        lexeme.line, error
+                                    )
+                                )
+
                         for token in self.token_list:
-                            file.write("{} line {} cols {}-{} is {}".format(token.lexeme, token.line, token.col_start, token.col_finish, token.category))
-
-                    else:
-                        for lexeme in lexeme_list:
-                            for error in token_obj.errors:
-                                if error.endswith(lexeme.word):
-                                    file.write("*** Error line {} *** {}".format(lexeme.line, error))
-
-                            for token in self.token_list:
-                                if lexeme.word == token.lexeme:
-                                    file.write("{} line {} cols {}-{} is {}".format(token.lexeme, token.line, token.col_start, token.col_finish, token.category))
-
+                            if lexeme.word == token.lexeme:
+                                file.write(
+                                    "{} line {} cols {}-{} is {}".format(
+                                        token.lexeme,
+                                        token.line,
+                                        token.col_start,
+                                        token.col_finish,
+                                        token.category,
+                                    )
+                                )
 
         except Exception:
             print("ERROR: could not create file")
