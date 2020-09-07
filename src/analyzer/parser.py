453309f5a1c89d9320978c39b087ed8917597c0a
Roberto Solares <betosolaresgar@gmail.com>
2020-09-07 10:06:06 -0600

    Add missing methods

--- src/analyzer/parser.py
+++ src/analyzer/parser.py
@@ -1,7 +1,34 @@
 class Parser:
-    def __init__(self, tokens):
+    def __init__(self):
+        self.__tokens__ = None
+        self.__errors__ = []
+
+    # Try to parse the tokens list, true if no errors
+    def try_parse(self, tokens):
         self.__tokens__ = iter(tokens)
+        # Parse method
+        return len(self.__errors__) == 0
+
+    # Get all the errors
+    def get_errors(self):
+        return self.__errors__
+
+    # Add a new error to the list
+    def __add_error__(self, token, expect):
+        exp = self.__multiple_expected__(expect) if len(expect) > 0 else expect
+        self.__errors__.append(
+            "*** Syntax Error *** on line "
+            + str(token.line)
+            + " "
+            + exp
+            + " was expected and got "
+            + token.word
+        )
 
     # Get the next token in the list
     def __get_next_token__(self):
         return next(self.__tokens__, None)
+
+    # Concatenate all the expected in a single string
+    def __multiple_expected__(self, expected):
+        return " or ".join(expected)
