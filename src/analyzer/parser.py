eb4599187885b7aea4c16395b25661714bd79b47
Roberto Solares <betosolaresgar@gmail.com>
2020-09-06 18:08:41 -0600

    Add method to add errors

--- src/analyzer/parser.py
+++ src/analyzer/parser.py
@@ -14,6 +14,24 @@ class Parser:
     def __get_next_token__(self):
         return next(self.__tokens__, None)
 
+    # Add a new error to the list of analysis
+    def __add_error__(self, token, expected):
+        self.__has_errors__ = True
+
+        if len(expected) > 0:
+            expect = self.__multiple_expected__(expected)
+        else:
+            expect = expected
+
+            self.__analysis__.append(
+                "*** Syntax Error *** on line "
+                + str(token.line)
+                + " "
+                + expect
+                + " was expected and got "
+                + token.word
+            )
+
     # Concatenate all the expected words in a single string
     def __multiple_expected__(self, expected):
         new_expected = ""
