015559710dd2792a101e2b4dfdbc8bc533b484e3
mochisvelas <velasquezmochis@gmail.com>
2020-11-18 00:46:22 -0600

    create symbol and symbols table

---
+++ src/analyzer/symbol.py
@@ -0,0 +1,29 @@
+class Symbol:
+    def __init__(self, type, category, value=None):
+        self.__type__ = type
+        self.__category__ = category
+        self.__value__ = value
+
+    @property
+    def type(self):
+        return self.__type__
+
+    @type.setter
+    def type(self, type):
+        self.__type__ = type
+
+    @property
+    def category(self):
+        return self.__category__
+
+    @type.setter
+    def category(self, category):
+        self.__category__ = category
+
+    @value
+    def value(self):
+        return self.__value__
+
+    @value.setter
+    def value(self, value):
+        self.__value__ = value
