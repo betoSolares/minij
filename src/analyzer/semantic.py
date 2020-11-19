3df5709589df72661f6e756c005c571cd2af63ca
mochisvelas <velasquezmochis@gmail.com>
2020-11-19 00:59:20 -0600

    add more validatios

--- src/analyzer/semantic.py
+++ src/analyzer/semantic.py
@@ -14,7 +14,11 @@ class Semantic:
     def errors(self):
         return self.__errors__
 
+    # Try analyze
     def analyze(self, tokens):
+        import pdb
+
+        pdb.set_trace()
         self.__input__ = tokens
 
         while True:
@@ -23,11 +27,32 @@ class Semantic:
                 break
 
             current = self.__input__[self.__position__]
+            print(
+                "*** Current word:",
+                current.word,
+                "Current category:",
+                current.category,
+                "***",
+            )
             self.__position__ += 1
+            print(self.__position__)
 
             if current.category == "Identifier":
+                lookahead = self.__input__[self.__position__]
+                print(
+                    "*** Lookahead word:",
+                    lookahead.word,
+                    "Lookahead category:",
+                    lookahead.category,
+                    "***",
+                )
+
+                if lookahead.category == "Identifier":
+                    continue
+
                 if self.__get_symbol__(current) is None:
                     self.__add_symbol__(current)
+
                 else:
                     self.__update_symbol__(current)
             else:
@@ -38,38 +63,56 @@ class Semantic:
     # Add new symbol to symbols table
     def __add_symbol__(self, token):
 
-        previous = self.__input__[self.__position__ - 1].word
-        next = self.__input__[self.__position__ + 1].word
+        previous = self.__input__[self.__position__ - 2]
+        next = self.__input__[self.__position__]
+        print(
+            "*** Previous word:",
+            previous.word,
+            "Previous category:",
+            previous.category,
+            "***",
+        )
+        print(
+            "*** Next word:", next.word, "Next category:", next.category, "***"
+        )
+        type = ""
+        category = ""
 
-        if previous == "class":
-            next = "Class"
+        if previous.word == "class":
+            type = category = "class"
 
-        elif previous == "interface":
-            next = "Interface"
+        elif previous.word == "interface":
+            type = category = "interface"
 
-        elif previous == "static":
-            next = "Static"
+        elif previous.word == "static":
+            type = category = "static"
 
-        elif next == "(":
-            next = "Function"
+        elif previous.category == "Identifier":
+            type = previous.word
+            category = "object"
+
+        elif next.word == "(":
+            type = previous.word
+            category = "function"
 
         else:
-            next = "Variable"
+            type = previous.word
+            category = "variable"
 
-        self.__symbols__[token.word] = Symbol(previous, next)
+        self.__symbols__[token.word] = Symbol(type, category)
 
         return
 
     # Return existing symbol
     def __get_symbol__(self, symbol):
-        return self.__symbols__.get(symbol.word, default=None)
+        return self.__symbols__.get(symbol.word, None)
 
     # Update existing symbol
     def __update_symbol__(self, symbol):
-        next = self.__input__[self.__position__ + 1].word
+        next = self.__input__[self.__position__].word
 
         if next == "=":
-            value = self.__input__[self.__position__ + 2].word
+            value = self.__input__[self.__position__].word
             self.__symbols__[symbol].value = value
             # current = self.__symbols__[symbol]
             # current.value = value
